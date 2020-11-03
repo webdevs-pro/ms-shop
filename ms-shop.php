cbedaa06798e1ef7525353f8e32e9b4694174837
Alex Ischenko <alex.ischenko@magnificsoft.com>
2020-11-03 19:38:31 +0200

    Initial commit

---
+++ ms-shop.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: MS Shop
+* Plugin URI: https://WPBrigade.com/wordpress/plugins/loginpress/
+* Description: LoginPress is the best <code>wp-login</code> Login Page Customizer plugin by <a href="https://wpbrigade.com/">WPBrigade</a> which allows you to completely change the layout of login, register and forgot password forms.
+* Version: 1.4.6
+* Author: WPBrigade
+* Author URI: https://WPBrigade.com/
+*/
